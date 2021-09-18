4b38c6b1daac9d22aa218b88f1dc5d597d8059d8
Lucas Emanuel <86731709+Lunmiel@users.noreply.github.com>
2021-09-17 22:36:04 -0300

    Update conexão.php

--- conexão.php
+++ conexão.php
@@ -6,7 +6,7 @@
     const senha = '';
 
     try {
-      $conectar = new PDO('mysql:host=localhost;dbname=login', user, senha, [PDO::MYSQL_ATTR_INIT_COMMAND =>  "SET NAMES 'UTF8'"]);
+      $conectar = new PDO('mysql:host='.host.';dbname='.dbname, user, senha, [PDO::MYSQL_ATTR_INIT_COMMAND =>  "SET NAMES 'UTF8'"]);
       $conectar->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     }catch (Exception $e) 
       echo 'Erro ao conectar ao banco de dados';
